77e29b92c7c74f17d0065d190ab69ae71d876ad2
KevinLeNozer <kevin.lenozer@gmail.com>
2022-07-08 15:49:40 +0200

    firstCommit

---
+++ src/main/java/org/example/entites/Amplificateurs.java
@@ -0,0 +1,15 @@
+package org.example.entites;
+
+
+public class Amplificateurs {
+
+    Tuner tuner;
+    LecteurDvd lecteurDvd;
+    LecteurCd lecteurCd;
+
+    private String cd;
+    private String dvd;
+
+
+
+}
---
+++ src/main/java/org/example/facade/FacadeHomeCinema.java
@@ -0,0 +1,16 @@
+package org.example.facade;
+
+public interface FacadeHomeCinema {
+
+    public void regarderFilm();
+
+    public void arreterFilm();
+
+    public void ecouterCd();
+
+    public void arreterCd();
+
+    public void ecouterRadio();
+
+    public void arreterRadio();
+}
